37cd4ff009250e2bf79f14ccdea4bc6997ecb6a0
SharukhFardin <sharukhfardin1234@gmail.com>
2023-05-28 06:15:06 +0600

    Connected the OpenAI API.

--- website/views.py
+++ website/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.contrib import messages
+import openai
 # Create your views here.
 
+
+# API key = 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
+
+
 def home(request):
+
 	# list of coding language for codeGPT
 	lang_list = ['c', 'clike', 'cpp', 'csharp', 'css', 'dart', 'django', 'go', 'html', 'java', 'javascript', 'kotlin', 'markup', 'matlab', 'mongodb', 'objectivec', 'perl', 'php', 'plsql', 'powershell', 'python', 'r', 'regex', 'ruby', 'rust', 'sql', 'swift', 'yaml']
 
@@ -15,7 +21,26 @@ def home(request):
 			messages.success(request, "Hey! Please select a programming language.")
 			return render(request, 'home.html', {'lang_list':lang_list, 'code':code, 'lang':lang})
 
-		return render(request, 'home.html', {'lang_list':lang_list, 'code':code, 'lang':lang})
+		else:
+			#OpenAI Key!!!!
+			openai.api_key = "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"
+			# Create OpenAI instance
+			openai.Model.list()
+			# Make an OpenAI Request
+			try:
+				response = openai.Completion.create(
+					engine = 'text-davinci-003',
+					prompt = f"Respond only with code. Fix this {lang} code: {code}",
+					temperature = 0,
+					max_tokens = 1000,
+					top_p = 1.0,
+					frequency_penalty = 0.0,
+					presence_penalty = 0.0,
+					)
+				return render(request, 'home.html', {'lang_list':lang_list, 'response':response, 'lang':lang})
+
+			except Exception as e:
+				return render(request, 'home.html', {'lang_list':lang_list, 'response':e, 'lang':lang})
 
 
 	return render(request, 'home.html', {'lang_list':lang_list})
